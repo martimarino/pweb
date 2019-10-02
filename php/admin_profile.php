27f977f93898bc669310e80488f706c4913e1600
martimarino <martina.marino1996@gmail.com>
2019-10-02 19:24:12 +0200

    admin profile boxes

--- php/admin_profile.php
+++ php/admin_profile.php
@@ -11,12 +11,52 @@
 <html>
 	<head>
 		<title>Il tuo profilo</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
+		<link href="../css/header.css" rel="stylesheet" type="text/css">
+		<link href="../css/admin.css" rel="stylesheet" type="text/css">
+	    <link rel="icon" href = "../immagini/supernova.png" sizes="32x32" type="image/png">
+	    <link href="https://fonts.googleapis.com/css?family=Srisakdi:700" rel="stylesheet">
+	    <link href="https://fonts.googleapis.com/css?family=Marmelad" rel="stylesheet">
+	    <script src="./../js/home.js"></script>  
+		<title>Supernova-Admin profile</title>
 	</head>
-	<body>
+	<body onLoad="hideBadges()">
+		<?php
+			include"./layout/top_bar.php";
+		?>
 		<div id="profile">
-		<b id="welcome">Welcome : <i><?php echo $_SESSION['username']; ?></i></b>
-		<b id="logout"><a href="./util/logout.php">Log Out</a></b>
+			<b id="welcome">Welcome : <i><?php echo $_SESSION['username']; ?></i></b>
+			<b id="logout"><a href="./util/logout.php">Log Out</a></b>
 		</div>
+			<ul class="admin_list">
+				<li>
+					<div class="container">
+						<h3>Insert into catalog</h3>
+					</div>
+				</li>
+				<li>
+					<div class="container">
+						<h3>Modify garment</h3>
+					</div>
+				</li>
+				<li>
+					<div class="container">
+						<h3>Delete from catalog</h3>
+					</div>
+				</li>
+				<li>
+					<div class="container">
+						<h3>Insert sale</h3>
+					</div>
+				</li>
+				<li>
+					<div class="container">
+						<h3>Modify order</h3>
+					</div>
+				</li>
+				<li>
+					<div class="container">
+					</div>
+				</li>
+			</ul>
 	</body>
 </html>
