cd20df326ef0bb2115c80bc42c149fa590a4c45e
martimarino <martina.marino1996@gmail.com>
2019-12-20 18:40:51 +0100

    Inserito link al catalogo dalla pagina personale

--- php/layout/profile_menu.php
+++ php/layout/profile_menu.php
@@ -8,10 +8,11 @@
 			?>
 		</p>
 		<ul>
+			<li><a id="catalog_link" href="./catalog.php" class="li_link">Go to catalog</a></li>
 			<li><a id="profile_informations_link" href="./profileInformations.php" class="li_link">Profile informations</a></li>
 			<li><a id="orders_link" href="./orders.php" class="li_link">Orders</a></li>
 			<li><a id="wish_list_link" href="./wishList.php" class="li_link">Whish List</a></li>
-			<li><a id="my_measures_link" href="./myMeasures.php" class="li_link">My measures</a></li>
+			<li><a id="my_measures_link" href="./myMeasures.php" class="li_link">How to choose the size</a></li>
 		</ul>
 	</nav>
 	<button type="submit" id="logout" onclick="location.href='./util/logout.php';">Log out</button>
